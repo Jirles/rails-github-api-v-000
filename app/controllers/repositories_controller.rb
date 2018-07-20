0320dbef1670709cf5c052723e06ec95ac007971
Emily Jirles <emily.jirles@gmail.com>
2018-07-20 12:56:45 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,6 +1,8 @@
 class RepositoriesController < ApplicationController
 
   def index
+    @user_resp = Faraday.get 'https://api.github.com/user' do |req|
+      req.headers['Authorization'] = "token #{session[:token]}"
     @resp = Faraday.get 'https://api.github.com/user/repos' do |req|
       req.headers['Accept'] = 'application/json'
       req.headers['Authorization'] = "token #{session[:token]}"
